84c4b2263c430c040dc994322180086cbd5df011
misostack <misostack.com@gmail.com>
2025-04-06 17:18:45 +0700

    Init nodejs typescript

---
+++ out/main.js
@@ -0,0 +1,12 @@
+var message = "Hello, TypeScript!";
+console.log(message);
+// process.on("SIGINT", () => {
+//   console.log("Caught interrupt signal");
+//   process.exit();
+// });
+// process.on("SIGTERM", () => {
+//   console.log("Caught terminate signal");
+//   process.exit();
+// });
+// process.exit(0);
+//# sourceMappingURL=main.js.map
